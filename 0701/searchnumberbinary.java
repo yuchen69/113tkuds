84914c0b3e77934b5d9a546caee7fd53d511284f
TKU-AI-411630998 <yc52085@gmail.com>
2025-07-01 12:46:38 +0800

    0701

--- 0701/searchnumberbinary.java
+++ 0701/searchnumberbinary.java
@@ -1,14 +1,18 @@
+import java.util.Scanner;
+
 public class searchnumberbinary {
     public static void main (String[] args){
-        System.out.println("請輸入要查找的數字:");
-        int X = scanner.nextInt();
+        int[] s = {1,3,5,7,9,11,13};
+        Scanner scanner = new Scanner(System.in);
 
-        int left = 0 
+        System.out.println("請輸入要查找的數字:");
+        int x = scanner.nextInt();
+
+        int left = 0;
         int right = s.length - 1;
         boolean found = false;
 
         while (left <= right){
-
             int mid = (left + right) / 2;
             System.out.println("比對中 : x =" + x + ",中間元素 = " + s[mid]);
 
@@ -18,8 +22,14 @@ public class searchnumberbinary {
             }else if (s[mid] < x){
                 left = mid + 1;
             }else {
-                right = mid - 2;
+                right = mid - 1;
         }
     }
+    if (found){
+        System.out.println("Yes");
+    }else{
+        System.out.println("No");
+    }
+    scanner.close();
     }
 }
