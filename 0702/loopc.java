f56218c1fda05489b19974123a7845c63858a462
TKU-AI-411630998 <yc52085@gmail.com>
2025-07-02 10:14:18 +0800

    add

---
+++ 0702/loopc.java
@@ -0,0 +1,11 @@
+public class loopc {
+    public static void main(String[] args) {
+        int x =0;
+        int count = 0;
+        for (int i = 1; 1 <= 100; i += 2){
+            x = x + 1;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
